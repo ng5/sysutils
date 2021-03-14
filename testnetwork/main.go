030b19cb895d1b0c11a69c39eea91bfba01f1bfb
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2021-03-14 22:53:06 +0000

    run tests concurrently

--- testnetwork/main.go
+++ testnetwork/main.go
@@ -85,14 +85,96 @@ func GetMaxRoutines(m map[string]shared.Row) int {
 	}
 	return maxRoutines
 }
+func RemoteExec(overwrite bool, row *shared.Row, csvFileBase string, ex string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	exPath, err := filepath.Abs(ex)
+	exBaseName := filepath.Base(ex)
+	if overwrite == true {
+		err := shared.TransferFile(row.SourceUser, row.Source, ":22", row.SourceKey, csvFileBase, "/tmp/"+csvFileBase)
+		if err != nil {
+			printRow(row, err)
+			return
+		}
+		err = shared.TransferFile(row.SourceUser, row.Source, ":22", row.SourceKey, exPath, "/tmp/"+exBaseName)
+		if err != nil {
+			printRow(row, err)
+			return
+		}
+	}
+	fmt.Println("running concurrent tests on " + row.Source)
+	err = shared.RemoteExecSSH(row.SourceUser, row.Source, ":22", row.SourceKey, "chmod +x /tmp/"+exBaseName)
+	if err != nil {
+		printRow(row, err)
+		return
+	}
+	err = shared.RemoteExecSSH(row.SourceUser, row.Source, ":22", row.SourceKey, "/tmp/"+exBaseName+" --file "+"/tmp/"+csvFileBase)
+	if err != nil {
+		printRow(row, err)
+		return
+	}
+}
+func LocalExecCurrent(row *shared.Row, wg *sync.WaitGroup) {
+	defer wg.Done()
+	timeout := time.Duration(row.TimeOut) * time.Millisecond
+	d := net.Dialer{Timeout: timeout}
+	protocol := strings.ToLower(row.Protocol)
+	if protocol == "multicast" {
+		protocol = "udp"
+	}
+	status := ""
+	if !IsLocal(row.Source) {
+		return
+	}
+	if strings.ToLower(row.Protocol) == "multicast" {
+		err := shared.MulticastRead(row.TargetIP, row.TargetPort, timeout, false)
+		if err != nil {
+			status = "FAILED: " + err.Error()
+		} else {
+			status = "OK"
+		}
+	} else {
+		conn, err := d.Dial(protocol, row.TargetIP+":"+row.TargetPort)
+		if err != nil {
+			status = "FAILED: " + err.Error()
+		} else {
+			conn.SetWriteDeadline(time.Now().Add(timeout))
+			_, err = conn.Write([]byte("test\n"))
+			if err != nil {
+				status = "FAILED " + err.Error()
+			} else {
+				status = "WRITE OK "
+				conn.SetReadDeadline(time.Now().Add(timeout))
+				_, _, err = bufio.NewReader(conn).ReadLine()
+				if err != nil {
+					status = status + "/ READ FAILED " + err.Error()
+				} else {
+					status = "OK"
+				}
+			}
+		}
+		if conn != nil {
+			_ = conn.Close()
+		}
+	}
+	fmt.Printf("%-12s %-20s %-20s %-20s %-12s %-12s\n", row.Description, hostName, row.Source, row.TargetIP+":"+row.TargetPort, row.Protocol, status)
+}
+func LocalExec(rows []shared.Row) {
+	fmt.Printf("%-12s %-20s %-20s %-20s %-12s %-12s\n", "Description", "HostName", "Source", "Target", "Protocol", "Status")
+	fmt.Printf("-------------------------------------------------------------------------------------------------------\n")
+	var wg sync.WaitGroup
+	wg.Add(len(rows))
+	for i, _ := range rows {
+		currentRow := rows[i]
+		go LocalExecCurrent(&currentRow, &wg)
+	}
+	wg.Wait()
+}
 func main() {
 	hostName, _ = os.Hostname()
 	ex, err := os.Executable()
 	if err != nil {
 		panic(err)
 	}
-	exPath, err := filepath.Abs(ex)
-	exBaseName := filepath.Base(ex)
 	csvFile := flag.String("file", "", "csv file containing rules")
 	remote := flag.Bool("remote", false, "run test on remote machines")
 	overwrite := flag.Bool("overwrite", true, "overwrite rules file and itself on remote machines")
@@ -120,81 +202,11 @@ func main() {
 			if IsLocal(row.Source) {
 				continue
 			}
-			go func() {
-				if *overwrite == true {
-					err := shared.TransferFile(row.SourceUser, row.Source, ":22", row.SourceKey, csvFileBase, "/tmp/"+csvFileBase)
-					if err != nil {
-						printRow(&row, err)
-						return
-					}
-					err = shared.TransferFile(row.SourceUser, row.Source, ":22", row.SourceKey, exPath, "/tmp/"+exBaseName)
-					if err != nil {
-						printRow(&row, err)
-						return
-					}
-				}
-				fmt.Println("running concurrent tests on " + row.Source)
-				err = shared.RemoteExecSSH(row.SourceUser, row.Source, ":22", row.SourceKey, "chmod +x /tmp/"+exBaseName)
-				if err != nil {
-					printRow(&row, err)
-					return
-				}
-				err = shared.RemoteExecSSH(row.SourceUser, row.Source, ":22", row.SourceKey, "/tmp/"+exBaseName+" --file "+"/tmp/"+csvFileBase)
-				if err != nil {
-					printRow(&row, err)
-					return
-				}
-				wg.Done()
-			}()
+			go RemoteExec(*overwrite, &row, csvFileBase, ex, &wg)
 		}
 		wg.Wait()
 	} else {
-		fmt.Printf("%-12s %-20s %-20s %-20s %-12s %-12s\n", "Description", "HostName", "Source", "Target", "Protocol", "Status")
-		fmt.Printf("-------------------------------------------------------------------------------------------------------\n")
-		for _, row := range rows {
-			timeout := time.Duration(row.TimeOut) * time.Millisecond
-			d := net.Dialer{Timeout: timeout}
-			protocol := strings.ToLower(row.Protocol)
-			if protocol == "multicast" {
-				protocol = "udp"
-			}
-			status := ""
-			if !IsLocal(row.Source) {
-				continue
-			}
-			if strings.ToLower(row.Protocol) == "multicast" {
-				err := shared.MulticastRead(row.TargetIP, row.TargetPort, timeout, false)
-				if err != nil {
-					status = "FAILED: " + err.Error()
-				} else {
-					status = "OK"
-				}
-			} else {
-				conn, err := d.Dial(protocol, row.TargetIP+":"+row.TargetPort)
-				if err != nil {
-					status = "FAILED: " + err.Error()
-				} else {
-					conn.SetWriteDeadline(time.Now().Add(timeout))
-					_, err = conn.Write([]byte("test\n"))
-					if err != nil {
-						status = "FAILED " + err.Error()
-					} else {
-						status = "WRITE OK "
-						conn.SetReadDeadline(time.Now().Add(timeout))
-						_, _, err = bufio.NewReader(conn).ReadLine()
-						if err != nil {
-							status = status + "/ READ FAILED " + err.Error()
-						} else {
-							status = "OK"
-						}
-					}
-				}
-				if conn != nil {
-					_ = conn.Close()
-				}
-			}
-			fmt.Printf("%-12s %-20s %-20s %-20s %-12s %-12s\n", row.Description, hostName, row.Source, row.TargetIP+":"+row.TargetPort, row.Protocol, status)
-		}
+		LocalExec(rows)
 	}
 
 }
