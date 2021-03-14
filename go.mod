c91b1cd7d43d7400167fb5c156b0b9fe5b4b8de1
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2021-03-14 19:23:31 +0000

    listen multicast traffic

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/panjf2000/gnet v1.4.2
 	github.com/pkg/sftp v1.13.0
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
 	golang.org/x/sys v0.0.0-20210313110737-8e9fff1a3a18 // indirect
 )
--- testlistener/main.go
+++ testlistener/main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/panjf2000/gnet"
 	"github.com/panjf2000/gnet/pool/goroutine"
+	"golang.org/x/net/ipv4"
 	"log"
+	"net"
+	"os"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
 
-type echoServer struct {
+type tcpServer struct {
 	*gnet.EventServer
 	pool *goroutine.Pool
 }
 
-func (es *echoServer) React(frame []byte, c gnet.Conn) (out []byte, action gnet.Action) {
+func (es *tcpServer) React(frame []byte, c gnet.Conn) (out []byte, action gnet.Action) {
 	data := append([]byte{}, frame...)
 	_ = es.pool.Submit(func() {
 		log.Println("TCP packet: " + strings.ReplaceAll(string(data), "\n", ""))
@@ -37,35 +43,108 @@ func (udp *udpServer) React(frame []byte, c gnet.Conn) (out []byte, action gnet.
 	return
 }
 
-type multicastServer struct {
-	*gnet.EventServer
-	pool *goroutine.Pool
+func mcastRead(group, port string) {
+	p, err1 := strconv.Atoi(port)
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	a := net.ParseIP("0.0.0.0")
+	g := net.ParseIP(group)
+	if g == nil {
+		log.Fatal(fmt.Errorf("bad group: '%s'", group))
+	}
+	c, err3 := mcastOpen(a, p)
+	if err3 != nil {
+		log.Fatal(err3)
+	}
+	if err := c.JoinGroup(nil, &net.UDPAddr{IP: g}); err != nil {
+		log.Fatal(err)
+	}
+	if err := c.SetControlMessage(ipv4.FlagTTL|ipv4.FlagSrc|ipv4.FlagDst|ipv4.FlagInterface, true); err != nil {
+		log.Fatal(err)
+	}
+	readLoop(c)
+	c.Close()
 }
 
-func (multicast *multicastServer) React(frame []byte, c gnet.Conn) (out []byte, action gnet.Action) {
-	data := append([]byte{}, frame...)
-	_ = multicast.pool.Submit(func() {
-		log.Println("MULTICAST packet: " + strings.ReplaceAll(string(data), "\n", ""))
-	})
-	return
+func mcastOpen(bindAddr net.IP, port int) (*ipv4.PacketConn, error) {
+	s, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, syscall.IPPROTO_UDP)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := syscall.SetsockoptInt(s, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
+		log.Fatal(err)
+	}
+	lsa := syscall.SockaddrInet4{Port: port}
+	copy(lsa.Addr[:], bindAddr.To4())
+	if err := syscall.Bind(s, &lsa); err != nil {
+		syscall.Close(s)
+		log.Fatal(err)
+	}
+	f := os.NewFile(uintptr(s), "")
+	c, err := net.FilePacketConn(f)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	p := ipv4.NewPacketConn(c)
+	return p, nil
 }
+
+func readLoop(c *ipv4.PacketConn) {
+	buf := make([]byte, 10000)
+	for {
+		n, cm, _, err1 := c.ReadFrom(buf)
+		if err1 != nil {
+			log.Printf("MULTICAST: readfrom: error %v", err1)
+			break
+		}
+		var name string
+		ifi, err2 := net.InterfaceByIndex(cm.IfIndex)
+		if err2 != nil {
+			log.Printf("readLoop: unable to solve ifIndex=%d: error: %v", cm.IfIndex, err2)
+		}
+
+		if ifi == nil {
+			name = "ifname?"
+		} else {
+			name = ifi.Name
+		}
+		log.Printf("MULTICAST packet: recv %d bytes from %s to %s on %s", n, cm.Src, cm.Dst, name)
+	}
+}
+
 func main() {
+	USAGE := "Usage: testlistener -t <TCP> -u <UDP> -m <MULTICAST GROUP:PORT>"
+	t := flag.String("t", "12001", "tcp port")
+	u := flag.String("u", "12002", "udp port")
+	m := flag.String("m", "239.0.0.1:12003", "multicast group & port")
+	flag.Parse()
+	if len(*t) == 0 || len(*u) == 0 || len(*m) == 0 {
+		fmt.Println(USAGE)
+		return
+	}
+	tokens := strings.Split(*m, ":")
+	if len(tokens) != 2 {
+		fmt.Println(USAGE)
+		return
+	}
 	p1 := goroutine.Default()
 	defer p1.Release()
 	p2 := goroutine.Default()
 	defer p2.Release()
-	p3 := goroutine.Default()
-	defer p3.Release()
 
-	tcp := 12001
-	udp := 12002
-	multicast := 12003
-	go gnet.Serve(&echoServer{pool: p1}, "tcp://:"+strconv.Itoa(tcp))
-	go gnet.Serve(&udpServer{pool: p2}, "udp://:"+strconv.Itoa(udp))
-	go gnet.Serve(&multicastServer{pool: p3}, "udp://224.0.0.1:"+strconv.Itoa(multicast))
-	log.Printf("listening TCP: %d\n", tcp)
-	log.Printf("listening UDP: %d\n", udp)
-	log.Printf("listening MULTICAST: %d\n", multicast)
+	tcp := "tcp://:" + *t
+	udp := "udp://:" + *u
+
+	go gnet.Serve(&tcpServer{pool: p1}, tcp)
+	go gnet.Serve(&udpServer{pool: p2}, udp)
+	go func() {
+		mcastRead(tokens[0], tokens[1])
+	}()
+	log.Printf("listening TCP: %s\n", *t)
+	log.Printf("listening UDP: %s\n", *u)
+	log.Printf("listening MULTICAST: %s\n", *m)
 	for {
 		time.Sleep(10 * time.Second)
 	}
